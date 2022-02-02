c1be336efc2dab07b69273d00c78606ae2fc38c2
Luis Mario <lmfg06@gmail.com>
2022-02-02 15:40:59 -0500

    Funcionalidad

--- src/containers/Todo.js
+++ src/containers/Todo.js
@@ -2,48 +2,48 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import styled from 'styled-components';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faSearch} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
-const Todo  =  () => {
- 
- 
- 
-  	return (
+const Todo = () => {
 
-<div>
 
-  {/* Navbar */}
-  <nav class="navbar navbar-light ">
-    <div class="container-fluid">
-    <a class="navbar-brand" href ><img src="./imagenes/logo.svg" width="80px" alt=""></img></a>
-    <form class="d-flex">   
-    <img src="https://cdn-icons.flaticon.com/png/512/2366/premium/2366367.png?token=exp=1642868532~hmac=6dd18ca8bd0e6a91785dd9e272ecb4e8" alt="" width="40px"/>
-      
-    </form>
+
+  return (
+
+    <div>
+
+      {/* Navbar */}
+      <nav className="navbar navbar-light ">
+        <div className="container-fluid">
+          <a className="navbar-brand"><img src="./imagenes/logo.svg" width="80px" alt=""></img></a>
+          <form className="d-flex">
+            <img src="https://cdn-icons.flaticon.com/png/512/2366/premium/2366367.png?token=exp=1642868532~hmac=6dd18ca8bd0e6a91785dd9e272ecb4e8" alt="" width="40px" />
+
+          </form>
+        </div>
+      </nav>
+
+
+      {/* Titulo */}
+      <Container>
+        <Title>Nada como una Guajolota para empezar el dìa</Title>
+      </Container><br></br>
+
+      {/* barra de busqueda */}
+      <ContainerBuscar>
+        <BarraBuscar >
+          <FontAwesomeIcon icon={faSearch} />
+          <Busqueda type="text" placeholder="Sabor de guajolota, bebida..." />
+        </BarraBuscar>
+      </ContainerBuscar>
+
+      {/* Productos  */}
+
+
     </div>
-  </nav>
-
-
-{/* Titulo */}
-    <Container>
-      <Title>Nada como una Guajolota para empezar el dìa</Title>
-    </Container><br></br>
-
-{/* barra de busqueda */}
-    <ContainerBuscar>
-                <BarraBuscar >
-                <FontAwesomeIcon icon={faSearch}/>
-                    <Busqueda type="text" placeholder="Sabor de guajolota, bebida..."/>           
-                </BarraBuscar>
-    </ContainerBuscar> 
-
-{/* Productos  */}
-    
-
-</div>
-  	)
-  };
+  )
+};
 
 const Title = styled.h2`
 text-align: center;
@@ -78,5 +78,5 @@ outline: none;
 // `
 
 //const  = styled.a ``
- 
+
 export default Todo;
--- src/routers/AppRouters.js
+++ src/routers/AppRouters.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from '../App';
 import Bebidas from '../containers/Bebidas';
@@ -8,16 +8,26 @@ import Tamales from '../containers/Tamales';
 
 
 const AppRouters = () => {
+  const url = 'https://apiguajolotas.herokuapp.com/guapjolotas/'
+  const [todos, setTodos] = useState()
+  const fetchApi = async () => {
+    const response = await fetch(url)
+    const responseJSON = await response.json()
+    setTodos(responseJSON)
+  }
+  useEffect(() => {
+    fetchApi()
+  }, [])
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />}/>
-            <Route path="/Nada" element={<Nada />}/>
-            <Route path="/Guajolo" element={<Guajolo/>} />
-            <Route path="/Bebidas" element={<Bebidas/>} />
-            <Route path="/Tamales" element={<Tamales/>} />
-            <Route path="/" element={<Nada/>} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Nada/:id" element={<Nada todos={todos} />} />
+        <Route path="/Guajolo" element={<Guajolo todos={todos} />} />
+        <Route path="/Bebidas" element={<Bebidas />} />
+        <Route path="/Tamales" element={<Tamales />} />
+        <Route path="/" element={<Nada />} />
+      </Routes>
     </BrowserRouter>
   )
 };
